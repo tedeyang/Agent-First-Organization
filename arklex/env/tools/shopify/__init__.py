cb0afdd946d296354841585f025aeefec2ce189b
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-02 16:54:07 -0400

    Update __init__.py

---
+++ arklex/env/tools/shopify/__init__.py
@@ -0,0 +1,5 @@
+"""
+Shopify tools package for the Arklex framework.
+
+This package contains tool implementations for e-commerce operations and Shopify API integration in the Arklex framework.
+"""
