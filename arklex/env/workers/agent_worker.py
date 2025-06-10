f33670a240c3e13d8a33c84523d9790b1f85c2d2
Vishruth Devan <vishruth.devan@arklex.ai>
2025-06-10 13:16:00 -0700

    implement end_conversation as an openai tool

--- arklex/env/workers/agent_worker.py
+++ arklex/env/workers/agent_worker.py
@@ -1,14 +1,11 @@
+import json
 import logging
 import traceback
+from functools import partial
 from typing import Any, Dict, Optional
 
-from langchain.prompts import PromptTemplate
-from langchain_core.language_models import BaseChatModel
-from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
-from langgraph.graph import START, StateGraph
-
 from arklex.env.prompts import load_prompts
+from arklex.env.tools.tools import register_tool
 from arklex.env.tools.utils import trace
 from arklex.env.workers.worker import BaseWorker, register_worker
 from arklex.types import EventType, StreamType
@@ -16,79 +13,61 @@ from arklex.utils.graph_state import MessageState, StatusEnum
 from arklex.utils.model_config import MODEL
 from arklex.utils.model_provider_config import PROVIDER_MAP
 from arklex.utils.utils import chunk_string
+from langchain.prompts import PromptTemplate
+from langchain_core.language_models import BaseChatModel
+from langchain_core.messages.ai import AIMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langgraph.graph import START, StateGraph
 
 logger = logging.getLogger(__name__)
+
+
+@register_tool("Ends the conversation with a thank you message.", isResponse=True)
+def end_conversation(state: MessageState) -> MessageState:
+    logger.info("Ending the conversation.")
+    state.response = "Thank you for using Arklex. Goodbye!"
+    state.status = StatusEnum.COMPLETE
+    state.message_flow = ""
+    return state
 
 
 @register_worker
 class AgentWorker(BaseWorker):
     description: str = "General-purpose Arklex agent worker for chat or voice."
 
-    def __init__(self) -> None:
+    def __init__(self, successors: list, predecessors: list, tools: list) -> None:
         super().__init__()
         self.action_graph: StateGraph = self._create_action_graph()
         self.llm: Optional[BaseChatModel] = None
-        self.actions = {
-            "generate": "Answer a question or generate a response based on the user input.",
-            "end_conversation": "End the conversation",
-        }
 
-    def verify_action(self, state: MessageState, **kwargs: Any) -> str:
-        
-        logger.info("\n\n\nkwargs for agent worker execution:")
-        logger.info(kwargs)
+        self.available_tools: dict[str, Any] = {}
+        self.available_workers: dict[str, Any] = {}
 
-        
-        agent_task: str = state.orchestrator_message.attribute.get("task", "")
-        actions_info: str = "\n".join(
-            [f"{name}: {description}" for name, description in self.actions.items()]
-        )
-        actions_name: str = ", ".join(self.actions.keys())
-        logger.info(state)
+        for node in successors + predecessors:
+            if node.resource_id in tools:
+                self.available_tools[node.resource_id] = tools[node.resource_id]
 
-        prompts: Dict[str, str] = load_prompts(state.bot_config)
-        prompt: PromptTemplate = PromptTemplate.from_template(
-            prompts["agent_action_prompt"]
-        )
-        input_prompt = prompt.invoke(
-            {
-                "agent_task": agent_task,
-                "actions_info": actions_info,
-                "actions_name": actions_name,
-                "formatted_chat": state.user_message.history,
-            }
-        )
-        chunked_prompt: str = chunk_string(
-            input_prompt.text, tokenizer=MODEL["tokenizer"], max_length=MODEL["context"]
-        )
-        logger.info(f"Chunked prompt for deciding choosing DB action: {chunked_prompt}")
-        final_chain = self.llm | StrOutputParser()
-        try:
-            answer: str = final_chain.invoke(chunked_prompt)
-            for action_name in self.actions.keys():
-                if action_name in answer:
-                    logger.info(f"Chosen action in the database worker: {action_name}")
-                    return action_name
-            logger.info(f"Base action chosen in the database worker: Others")
-            return "generator"
-        except Exception as e:
-            logger.error(
-                f"Error occurred while choosing action in the database worker: {e}"
-            )
-            return "generator"
+        self.tool_map = {}
+        self.tool_defs = []
 
-    def end_conversation(self, state: MessageState) -> MessageState:
-        logger.info("Ending the conversation.")
-        state.response = "Thank you for using Arklex. Goodbye!"
-        state.status = StatusEnum.COMPLETE
-        state.message_flow = ""
-        return state
+        for tool_id, tool in self.available_tools.items():
+            tool_def = tool["execute"]().to_openai_tool_def()
+            self.tool_defs.append(tool_def)
+            self.tool_map[tool_def["function"]["name"]] = tool["execute"]()
 
-    def generator(self, state: MessageState) -> MessageState:
+        end_conversation_tool = end_conversation()
+        end_conversation_tool_def = end_conversation_tool.to_openai_tool_def()
+        end_conversation_tool_def["function"]["name"] = "end_conversation"
+        self.tool_defs.append(end_conversation_tool_def)
+        self.tool_map["end_conversation"] = end_conversation_tool.func
+
+        logger.info(f"AgentWorker initialized with {len(self.tool_defs)} tools.")
+
+    def generate(self, state: MessageState) -> MessageState:
         logger.info("\nGenerating response using the agent worker.")
         user_message = state.user_message
         orchestrator_message = state.orchestrator_message
-        message_flow: str = state.response + "\n" + state.message_flow
 
         orch_msg_content: str = (
             "None" if not orchestrator_message.message else orchestrator_message.message
@@ -101,45 +80,49 @@ class AgentWorker(BaseWorker):
             return state
 
         prompts: Dict[str, str] = load_prompts(state.bot_config)
-        if message_flow and message_flow != "\n":
-            prompt: PromptTemplate = PromptTemplate.from_template(
-                prompts["message_flow_generator_prompt"]
-            )
-            input_prompt = prompt.invoke(
-                {
-                    "sys_instruct": state.sys_instruct,
-                    "message": orch_msg_content,
-                    "formatted_chat": user_message.history,
-                    "context": message_flow,
-                }
-            )
-        else:
-            prompt: PromptTemplate = PromptTemplate.from_template(
-                prompts["message_generator_prompt"]
-            )
-            input_prompt = prompt.invoke(
-                {
-                    "sys_instruct": state.sys_instruct,
-                    "message": orch_msg_content,
-                    "formatted_chat": user_message.history,
-                }
-            )
-        logger.info(f"Prompt: {input_prompt.text}")
-        final_chain = self.llm | StrOutputParser()
-        answer: str = final_chain.invoke(input_prompt.text)
 
-        state.message_flow = ""
-        state.response = answer
-        state = trace(input=answer, state=state)
+        prompt: PromptTemplate = PromptTemplate.from_template(
+            prompts["function_calling_agent_prompt"]
+        )
+        input_prompt = prompt.invoke(
+            {
+                "sys_instruct": state.sys_instruct,
+                "message": orch_msg_content,
+                "formatted_chat": user_message.history,
+            }
+        )
+
+        logger.info(f"Prompt: {input_prompt.text}")
+
+        final_chain = self.llm
+        ai_message: AIMessage = final_chain.invoke(input_prompt.text)
+
+        logger.info(f"Generated answer: {ai_message}")
+
+        if ai_message.tool_calls:
+            logger.info("Processing tool calls.")
+            for tool_call in ai_message.tool_calls:
+                tool_name = tool_call.get("name")
+                if tool_name in self.tool_map:
+                    tool_response = self.tool_map[tool_name](
+                        state=state, **tool_call.get("args")
+                    )
+                    if isinstance(tool_response, MessageState):
+                        state = tool_response
+                    else:
+                        state.response = tool_response
+                else:
+                    logger.warning(f"Tool {tool_name} not found in tool map.")
+        else:
+            state.message_flow = ""
+            state.response = ai_message.content
+            state = trace(input=ai_message.content, state=state)
         return state
 
     def _create_action_graph(self) -> StateGraph:
         workflow = StateGraph(MessageState)
-        workflow.add_node("generator", self.generator)
-        workflow.add_node("end_conversation", self.end_conversation)
-        workflow.add_conditional_edges(
-            START, lambda state, **kwargs: self.verify_action(state, **kwargs)
-        )
+        workflow.add_node("generate", self.generate)
+        workflow.add_edge(START, "generate")
 
         return workflow
 
@@ -147,6 +130,7 @@ class AgentWorker(BaseWorker):
         self.llm = PROVIDER_MAP.get(
             msg_state.bot_config.llm_config.llm_provider, ChatOpenAI
         )(model=msg_state.bot_config.llm_config.model_type_or_path)
+        self.llm = self.llm.bind_tools(self.tool_defs)
         graph = self.action_graph.compile()
         result: Dict[str, Any] = graph.invoke(msg_state)
         return result
@@ -164,4 +148,5 @@ class AgentWorker(BaseWorker):
         except Exception as e:
             logger.error(traceback.format_exc())
             msg_state.status = StatusEnum.INCOMPLETE
+            return msg_state
             return msg_state
