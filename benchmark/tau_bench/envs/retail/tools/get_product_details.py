ab56ea5aa06be41f1e8925eb52c10d2f3f7379da
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-27 18:11:32 -0400

    Update get_product_details.py

--- benchmark/tau_bench/envs/retail/tools/get_product_details.py
+++ benchmark/tau_bench/envs/retail/tools/get_product_details.py
@@ -1,20 +1,21 @@
 # Copyright Sierra
 
 import json
-from typing import Any, Dict
+from typing import Any
+
 from benchmark.tau_bench.envs.tool import Tool
 
 
 class GetProductDetails(Tool):
     @staticmethod
-    def invoke(data: Dict[str, Any], product_id: str) -> str:
+    def invoke(data: dict[str, Any], product_id: str) -> str:
         products = data["products"]
         if product_id in products:
             return json.dumps(products[product_id])
         raise Exception("Error: product not found")
 
     @staticmethod
-    def get_info() -> Dict[str, Any]:
+    def get_info() -> dict[str, Any]:
         return {
             "type": "function",
             "function": {
