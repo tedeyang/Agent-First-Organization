c37251a6814a5894baf9addd4922c45003cc315a
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-27 18:11:40 -0400

    Update list_all_product_types.py

--- benchmark/tau_bench/envs/retail/tools/list_all_product_types.py
+++ benchmark/tau_bench/envs/retail/tools/list_all_product_types.py
@@ -1,13 +1,14 @@
 # Copyright Sierra
 
 import json
-from typing import Any, Dict
+from typing import Any
+
 from benchmark.tau_bench.envs.tool import Tool
 
 
 class ListAllProductTypes(Tool):
     @staticmethod
-    def invoke(data: Dict[str, Any]) -> str:
+    def invoke(data: dict[str, Any]) -> str:
         products = data["products"]
         product_dict = {
             product["name"]: product["product_id"] for product in products.values()
@@ -16,7 +17,7 @@ class ListAllProductTypes(Tool):
         return json.dumps(product_dict)
 
     @staticmethod
-    def get_info() -> Dict[str, Any]:
+    def get_info() -> dict[str, Any]:
         return {
             "type": "function",
             "function": {
