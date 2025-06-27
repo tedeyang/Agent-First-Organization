b05970ab02725f9c72b8546aeb33af2843ac4595
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-27 18:13:05 -0400

    Update outlines_completion.py

--- benchmark/tau_bench/model_utils/model/outlines_completion.py
+++ benchmark/tau_bench/model_utils/model/outlines_completion.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional
+from typing import Any
 
 from pydantic import BaseModel
 
@@ -9,11 +9,11 @@ from benchmark.tau_bench.model_utils.model.vllm_utils import generate_request
 
 class OutlinesCompletionModel(VLLMCompletionModel):
     def parse_force_from_prompt(
-        self, prompt: str, typ: BaseModel, temperature: Optional[float] = None
-    ) -> Dict[str, Any]:
+        self, prompt: str, typ: BaseModel, temperature: float | None = None
+    ) -> dict[str, Any]:
         if temperature is None:
             temperature = self.temperature
-        schema: Dict[str, Any] = typ.model_json_schema()
+        schema: dict[str, Any] = typ.model_json_schema()
         res: str = generate_request(
             url=self.url,
             prompt=prompt,
