db2ba29231f24b335f714e131081484354d3b961
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-27 18:11:38 -0400

    Update modify_pending_order_address.py

--- benchmark/tau_bench/envs/retail/tools/modify_pending_order_address.py
+++ benchmark/tau_bench/envs/retail/tools/modify_pending_order_address.py
@@ -1,14 +1,15 @@
 # Copyright Sierra
 
 import json
-from typing import Any, Dict
+from typing import Any
+
 from benchmark.tau_bench.envs.tool import Tool
 
 
 class ModifyPendingOrderAddress(Tool):
     @staticmethod
     def invoke(
-        data: Dict[str, Any],
+        data: dict[str, Any],
         order_id: str,
         address1: str,
         address2: str,
@@ -37,7 +38,7 @@ class ModifyPendingOrderAddress(Tool):
         return json.dumps(order)
 
     @staticmethod
-    def get_info() -> Dict[str, Any]:
+    def get_info() -> dict[str, Any]:
         return {
             "type": "function",
             "function": {
