9f52052936ae4c0b35468c5303fefe96d6275711
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-28 12:19:14 -0400

    Update search_direct_flight.py

--- benchmark/tau_bench/envs/airline/tools/search_direct_flight.py
+++ benchmark/tau_bench/envs/airline/tools/search_direct_flight.py
@@ -1,28 +1,30 @@
 # Copyright Sierra
 
 import json
-from typing import Any, Dict
+from typing import Any
+
 from benchmark.tau_bench.envs.tool import Tool
 
 
 class SearchDirectFlight(Tool):
     @staticmethod
-    def invoke(data: Dict[str, Any], origin: str, destination: str, date: str) -> str:
+    def invoke(data: dict[str, Any], origin: str, destination: str, date: str) -> str:
         flights = data["flights"]
         results = []
         for flight in flights.values():
-            if flight["origin"] == origin and flight["destination"] == destination:
-                if (
-                    date in flight["dates"]
-                    and flight["dates"][date]["status"] == "available"
-                ):
-                    # results add flight except dates, but add flight["datas"][date]
-                    results.append({k: v for k, v in flight.items() if k != "dates"})
-                    results[-1].update(flight["dates"][date])
+            if (
+                flight["origin"] == origin
+                and flight["destination"] == destination
+                and date in flight["dates"]
+                and flight["dates"][date]["status"] == "available"
+            ):
+                # results add flight except dates, but add flight["datas"][date]
+                results.append({k: v for k, v in flight.items() if k != "dates"})
+                results[-1].update(flight["dates"][date])
         return json.dumps(results)
 
     @staticmethod
-    def get_info() -> Dict[str, Any]:
+    def get_info() -> dict[str, Any]:
         return {
             "type": "function",
             "function": {
