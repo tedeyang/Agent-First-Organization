7b79f0025988395e56bf20de20a460ab6152d045
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-28 08:57:29 -0400

    Update wiki.py

--- benchmark/tau_bench/envs/airline/wiki.py
+++ benchmark/tau_bench/envs/airline/wiki.py
@@ -4,5 +4,5 @@ import os
 
 FOLDER_PATH = os.path.dirname(__file__)
 
-with open(os.path.join(FOLDER_PATH, "wiki.md"), "r") as f:
+with open(os.path.join(FOLDER_PATH, "wiki.md")) as f:
     WIKI = f.read()
