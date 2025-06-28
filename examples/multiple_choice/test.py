3bc96e57effa708c659e5d2be8198a47b42b3087
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-28 11:05:33 -0400

    Update test.py

--- examples/multiple_choice/test.py
+++ examples/multiple_choice/test.py
@@ -2,16 +2,16 @@
 # Go to the parent folder of this file (multiple_choice), then Run python -m unittest test.py to test the code in this file.
 import json
 import unittest
-from typing import Any, Dict, List, Tuple
+from typing import Any
 
-from arklex.orchestrator.orchestrator import AgentOrg
 from arklex.env.env import Env
+from arklex.orchestrator.orchestrator import AgentOrg
 
 
 class Logic_Test(unittest.TestCase):
     file_path: str = "test_cases.json"
-    with open(file_path, "r", encoding="UTF-8") as f:
-        TEST_CASES: List[Dict[str, Any]] = json.load(f)
+    with open(file_path, encoding="UTF-8") as f:
+        TEST_CASES: list[dict[str, Any]] = json.load(f)
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -19,8 +19,8 @@ class Logic_Test(unittest.TestCase):
         cls.user_prefix: str = "user"
         cls.worker_prefix: str = "assistant"
         file_path: str = "taskgraph.json"
-        with open(file_path, "r", encoding="UTF-8") as f:
-            cls.config: Dict[str, Any] = json.load(f)
+        with open(file_path, encoding="UTF-8") as f:
+            cls.config: dict[str, Any] = json.load(f)
         cls.env: Env = Env(
             tools=cls.config.get("tools", []),
             workers=cls.config.get("workers", []),
@@ -32,31 +32,31 @@ class Logic_Test(unittest.TestCase):
         """Method to tear down the test fixture. Run AFTER the test methods."""
 
     def _get_api_bot_response(
-        self, user_text: str, history: List[Dict[str, str]], params: Dict[str, Any]
-    ) -> Tuple[str, Dict[str, Any]]:
-        data: Dict[str, Any] = {
+        self, user_text: str, history: list[dict[str, str]], params: dict[str, Any]
+    ) -> tuple[str, dict[str, Any]]:
+        data: dict[str, Any] = {
             "text": user_text,
             "chat_history": history,
             "parameters": params,
         }
         orchestrator = AgentOrg(config=self.config, env=self.env)
-        result: Dict[str, Any] = orchestrator.get_response(data)
+        result: dict[str, Any] = orchestrator.get_response(data)
 
         return result["answer"], result["parameters"]
 
     def test_Unittest0(self) -> None:
         print("\n=============Unit Test 0=============")
         print(f"Task descrption: {self.TEST_CASES[0]['description']}")
-        history: List[Dict[str, str]] = []
-        params: Dict[str, Any] = {}
-        nodes: List[str] = []
+        history: list[dict[str, str]] = []
+        params: dict[str, Any] = {}
+        nodes: list[str] = []
         for node in self.config["nodes"]:
             if node[1].get("type", "") == "start":
                 start_message: str = node[1]["attribute"]["value"]
                 break
         history.append({"role": self.worker_prefix, "content": start_message})
 
-        assistant_messages: List[str] = []
+        assistant_messages: list[str] = []
         for user_text in self.TEST_CASES[0]["user_utterance"]:
             print(f"User: {user_text}")
             output, params = self._get_api_bot_response(user_text, history, params)
