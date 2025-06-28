f1465a5897f24b8040f233f05ce223bc48a975f8
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-28 09:04:45 -0400

    Update search_onestop_flight.py

--- benchmark/tau_bench/envs/airline/tools/search_onestop_flight.py
+++ benchmark/tau_bench/envs/airline/tools/search_onestop_flight.py
@@ -1,13 +1,14 @@
 # Copyright Sierra
 
 import json
-from typing import Any, Dict
+from typing import Any
+
 from benchmark.tau_bench.envs.tool import Tool
 
 
 class SearchOnestopFlight(Tool):
     @staticmethod
-    def invoke(data: Dict[str, Any], origin: str, destination: str, date: str) -> str:
+    def invoke(data: dict[str, Any], origin: str, destination: str, date: str) -> str:
         flights = data["flights"]
         results = []
         for flight1 in flights.values():
@@ -27,26 +28,23 @@ class SearchOnestopFlight(Tool):
                             > flight2["scheduled_departure_time_est"]
                         ):
                             continue
-                        if date in flight1["dates"] and date2 in flight2["dates"]:
-                            if (
-                                flight1["dates"][date]["status"] == "available"
-                                and flight2["dates"][date2]["status"] == "available"
-                            ):
-                                result1 = {
-                                    k: v for k, v in flight1.items() if k != "dates"
-                                }
-                                result1.update(flight1["dates"][date])
-                                result1["date"] = date
-                                result2 = {
-                                    k: v for k, v in flight2.items() if k != "dates"
-                                }
-                                result2.update(flight2["dates"][date])
-                                result2["date"] = date2
-                                results.append([result1, result2])
+                        if (
+                            date in flight1["dates"]
+                            and date2 in flight2["dates"]
+                            and flight1["dates"][date]["status"] == "available"
+                            and flight2["dates"][date2]["status"] == "available"
+                        ):
+                            result1 = {k: v for k, v in flight1.items() if k != "dates"}
+                            result1.update(flight1["dates"][date])
+                            result1["date"] = date
+                            result2 = {k: v for k, v in flight2.items() if k != "dates"}
+                            result2.update(flight2["dates"][date])
+                            result2["date"] = date2
+                            results.append([result1, result2])
         return json.dumps(results)
 
     @staticmethod
-    def get_info() -> Dict[str, Any]:
+    def get_info() -> dict[str, Any]:
         return {
             "type": "function",
             "function": {
