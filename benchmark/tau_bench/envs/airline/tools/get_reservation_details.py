8405b9c1e9e080b0c223c748d4d4e58ed5545b18
Christian Yongwhan Lim <yongwhan@yongwhan.io>
2025-06-28 08:56:56 -0400

    Update get_reservation_details.py

--- benchmark/tau_bench/envs/airline/tools/get_reservation_details.py
+++ benchmark/tau_bench/envs/airline/tools/get_reservation_details.py
@@ -1,20 +1,21 @@
 # Copyright Sierra
 
 import json
-from typing import Any, Dict
+from typing import Any
+
 from benchmark.tau_bench.envs.tool import Tool
 
 
 class GetReservationDetails(Tool):
     @staticmethod
-    def invoke(data: Dict[str, Any], reservation_id: str) -> str:
+    def invoke(data: dict[str, Any], reservation_id: str) -> str:
         reservations = data["reservations"]
         if reservation_id in reservations:
             return json.dumps(reservations[reservation_id])
         return "Error: user not found"
 
     @staticmethod
-    def get_info() -> Dict[str, Any]:
+    def get_info() -> dict[str, Any]:
         return {
             "type": "function",
             "function": {
